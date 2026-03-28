3823a9ab4bcf8019d4f4a5e6461fe6a477adde4e
HXiong <asd475885437@gmail.com>
2026-03-27 20:18:18 -0400

    update connection to backend api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://personal-portfolio-backend-32s5.onrender.com/"
+  baseURL: "https://personal-portfolio-backend-32s5.onrender.com/api"
 });
 
 export default API;
