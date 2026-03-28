ce116fc149350844de9ae8dc5b28439c851a3b25
HXiong <asd475885437@gmail.com>
2026-03-27 20:12:48 -0400

    connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://personal-portfolio-backend-32s5.onrender.com/"
 });
 
 export default API;
